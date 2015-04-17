558f5e98102ed25088d0cd31f773342d548d7179
Luke Van Seters <lukevanseters@gmail.com>
2015-04-16 23:32:56 -0400

    Add seconds param.

--- waitfacts.py
+++ waitfacts.py
@@ -1,23 +1,27 @@
 from multiprocessing.pool import ThreadPool
-import json
 import requests
 import time
-import sys
 
-def waitfacts(f):
-    def inner():
-        pool = ThreadPool(processes=2)
-        t = pool.apply_async(func=f)
-        while not t.ready():
-            time.sleep(2)
-            print get_fact() + "\n"
-        print "\n"
-    return inner
-
+def waitfacts(num_seconds=5):
+    def waitfacts_(f):
+        def inner():
+            pool = ThreadPool(processes=2)
+            t = pool.apply_async(func=f)
+            while not t.ready():
+                time.sleep(num_seconds)
+                print get_fact() + "\n"
+            print "\n"
+        return inner
+    return waitfacts_
+ 
 def get_fact():
     """ Gets a fact from mentalfloss's fun facts API. """
-
+    
     url = "http://mentalfloss.com/api/1.0/views/amazing_facts.json?limit=1"
     r = requests.get(url)
-    raw = r.json()[0]['nid']
+    try:
+        raw = r.json()[0]['nid']
+    except NoneType:
+        return get_fact()
+
     return raw[3:-5]
